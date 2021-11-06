7fdd55cdd65adb7031a8280ac52d0cd932bbf798
Fernandoramos24 <87905334+Fernandoramos24@users.noreply.github.com>
2021-11-06 10:17:28 -0600

    clases

---
+++ carro.py
@@ -0,0 +1,10 @@
+class vehiculo:
+    def encender(self):
+        return"carro encendido"
+
+    def acelerar(self):
+        return"vehiculo en marcha"
+
+    def frenar(self):
+        return"frenar carro"
+    
